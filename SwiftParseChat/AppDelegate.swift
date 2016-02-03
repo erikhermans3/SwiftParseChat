6b49aa8707bfddd13ece5f1b5b367e0bc2199ad9
Erik Hermans <erikhermans3@outlook.com>
2016-02-03 20:22:10 +0100

    master branch

--- SwiftParseChat/AppDelegate.swift
+++ SwiftParseChat/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Erik Hermans on 2/20/15.
 //  Copyright (c) 2015 Erik Hermans. All rights reserved.
 //
+// master branch
 
 import UIKit
 // Parse and ParseFacebookUtils imported in SwiftParseChat-Bridging-Header.h
